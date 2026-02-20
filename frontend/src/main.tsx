c55b310999abdfca96ef29d960d5b20f5f1640ac
Nathaniel <excellentindevelopment@gmail.com>
2026-02-20 16:32:23 -0600

    Changing Dashboard.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// @ts-ignore: Cannot find module './index.css' or its corresponding type declarations.
-import './index.css'
+// @ts-ignore: allow importing CSS without type declarations
+import 'leaflet/dist/leaflet.css';
+// @ts-ignore: allow importing CSS without type declarations
+import './index.css';
 import App from './App.js'
 
 createRoot(document.getElementById('root')!).render(
