0af7764d445b4aadff29fd49616158fe056b01d3
Nathaniel <excellentindevelopment@gmail.com>
2026-02-20 18:59:11 -0600

    Connecting Dashboard.tsx to REACT_APP_BACKEND_API

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
   const [data, setData] = useState<any>(null);
 
   useEffect(() => {
-    const wsUrl = (globalThis as any).process?.env?.REACT_APP_BACKEND_WS 
+    const wsUrl = (globalThis as any).process?.env?.REACT_APP_BACKEND_API 
     // 'ws://localhost:8000/ws/live';
     const ws = new WebSocket(wsUrl);
     ws.onmessage = msg => setData(JSON.parse(msg.data) as any);
